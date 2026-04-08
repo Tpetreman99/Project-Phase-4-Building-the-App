83a2b5139553c9aee270b069b4e4d971d6a6fbf1
Tanner Petreman <tanner.petreman@edu.sait.ca>
2026-04-08 12:26:21 -0600

    added statsprovider to layout

--- pawn_to_power/app/_layout.tsx
+++ pawn_to_power/app/_layout.tsx
@@ -1,17 +1,20 @@
 import { Stack } from "expo-router";
 import { UserProvider } from ".././context/UserContext";
 import { AudioProvider } from "@/context/AudioContext";
+import { StatsProvider } from "@/context/StatsContext";
 
 export default function RootLayout() {
   return (
     <AudioProvider>
     <UserProvider>
+      <StatsProvider>
       <Stack
         initialRouteName="(screens)/index"
         screenOptions={{
           headerShown: false,
         }}
       />
+      </StatsProvider>
     </UserProvider>
     </AudioProvider>
   );
